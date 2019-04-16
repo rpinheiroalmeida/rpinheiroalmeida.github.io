296b51185c05381290cfd465a0567e5f49c303b2
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2019-04-16 00:26:09 -0300

    Refactoring

--- pelicanconf.py
+++ pelicanconf.py
@@ -70,10 +70,10 @@ FAVICON = '/images/favicon.ico'
 #         'related_posts',
 #     ]
 
-PLUGINS = [u"disqus_static"]
-DISQUS_SITENAME = u'https://rpinheiroalmeida.github.io'
-DISQUS_SECRET_KEY = u'Zr2wwjWfmeuHXbqEVEzt5ynTrJa3bF4azS8KkWRsBaTTn83fllnQApsn6YL6dJjY'
-DISQUS_PUBLIC_KEY = u'amdM1t932fTVvflet3hgBKjR5EgTVCtWCszMt6lrkRDRRVExfvgFnOvMo38vdTAm'
+# PLUGINS = [u"disqus_static"]
+# DISQUS_SITENAME = u'https://rpinheiroalmeida.github.io'
+# DISQUS_SECRET_KEY = u'Zr2wwjWfmeuHXbqEVEzt5ynTrJa3bF4azS8KkWRsBaTTn83fllnQApsn6YL6dJjY'
+# DISQUS_PUBLIC_KEY = u'amdM1t932fTVvflet3hgBKjR5EgTVCtWCszMt6lrkRDRRVExfvgFnOvMo38vdTAm'
 
 THEME = './temas/Flex'
 
