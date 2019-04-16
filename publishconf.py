4f1d3d2c026f0d69875b8e417dbb640420bce545
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2019-04-16 10:46:01 -0300

    Adding Google Analytics

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "rpinheiroalmeida"
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "UA-83683118-1"
